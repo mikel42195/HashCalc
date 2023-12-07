009a749d65c4c11bb21d72eb211da31a29b97894
mikel42195 <mvelasco15@alumno.uned.es>
2023-12-07 16:59:28 +0100

    beta version

---
+++ src/main/java/com/pipedreams/hashcalc/HashCalc.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pipedreams.hashcalc;
+
+import GUI.Pantalla;
+
+
+/**
+ *
+ * @author m1k3l
+ */
+public class HashCalc {
+
+    public static void main(String[] args) {
+        
+        
+        Pantalla miPantalla = new Pantalla();
+        miPantalla.setVisible(true);
+        miPantalla.setLocationRelativeTo(null);
+    }
+}
